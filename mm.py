dd07cba9cee31b2ba9a7cfc08c485e64731d80c4
xAmphibiAx <78476172+xAmphibiAx@users.noreply.github.com>
2023-08-24 20:12:06 +0300

    print

--- mm.py
+++ mm.py
@@ -2,7 +2,7 @@ import pyautogui
 import time
 
 
-print("Press Ctrl-C to quit.")
+print("Press Ctrl-C to quit. 3 Times")
 try:
     while True:
         length, width = pyautogui.size()
@@ -12,6 +12,10 @@ try:
             pyautogui.moveTo(currentMouseX + 1, None)
             currentMouseX, currentMouseY = pyautogui.position()
             pyautogui.moveTo(currentMouseX - 1, None)
+            localtime = time.localtime()
+            result = time.strftime("%I:%M:S %p", localtime)
+            currentMouseX, currentMouseY = pyautogui.position()
+            print('Moved at ' + str(result) + '. FromTo (' +str(currentMouseX) + ', ' +str(currentMouseY)+')')
         else:
             pyautogui.moveTo(currentMouseX - 1, None)
         time.sleep(90)
